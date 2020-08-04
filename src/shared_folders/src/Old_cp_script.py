50dd13cdef8d2d7e1091ca5965c60b58c0262fb1
Moises981 <jesus.castillo.j@tecsup.edu.pe>
2020-08-04 17:19:52 -0400

    Last

--- src/shared_folders/src/Old_cp_script.py
+++ src/shared_folders/src/Old_cp_script.py
@@ -1,19 +1,14 @@
 #! /usr/bin/env python
 import rospy
-from std_msgs.msg import Int8
+import os
 import subprocess
 import time
-
-
+rospy.init_node('CP_FILES')
 
 class Server(object):
 
     def __init__(self):
-        rospy.Subscriber('/Status',Int8,self.callback)
-        self.pub = rospy.Publisher('/Status',Int8,queue_size=1)
-        rospy.init_node('CP_FILES')
-        self.state = 0
-        self.SetState = 0
+
         #Argumento de la contrasena de ubuntu para sudo
         self.sudoPassword = '9596'
         #Comando para montar servidor en la carpeta con sudo
@@ -22,11 +17,10 @@ class Server(object):
         self.Shared_Folder_path = '//172.16.204.128/ros'
         self.Path_ubuntu = '/home/asus/Desktop/Test'
         self.IP_FOUND = False
+        self.Send = subprocess.Popen(['cp','-r','/home/asus/Desktop/Catkin_ws/src/shared_folders/Backup/','/home/asus/Desktop/Test/'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-    def callback(self , msg):
-        self.state = msg.data
 
-    def Search_ip(self):    #Buscar el Ip del servidor 
+    def Search_ip(self):
         device_list = subprocess.Popen(['timeout','1','df','-i'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         output , error = device_list.communicate()
         for line in output.split('\n'):
@@ -40,7 +34,7 @@ class Server(object):
 
     def Reconnection(self):
         #Remover tareas anteriores para evitar errores en desmontar la carpeta
-        self.CP_files.kill()
+        self.Send.kill()
         #Los comandas a ejecutar deben ser separados en arrays por argumentos
         p = subprocess.Popen(['sudo','-S','timeout','3','mount','-t','cifs','-o','username=Playtec,password=,dir_mode=0777,file_mode=0777',self.Shared_Folder_path,self.Path_ubuntu], stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
         ERR = p.communicate('{}\n'.format(self.sudoPassword))[1]
@@ -48,6 +42,7 @@ class Server(object):
         #Si se logro conectar al servidor
         if p.returncode == 0:
             rospy.loginfo('Connected to the server successfully')
+            self.Send
         
         #Sino si lanza un error vacio
         elif p.returncode!=0 and ERR!='': #Verificar si hay error
@@ -65,21 +60,14 @@ class Server(object):
     def Process(self):
         self.Search_ip()        #Buscar servidor en ubuntu
         if not self.IP_FOUND:   #Si no se encuentra intentara reconetarse
-            self.SetState = 4   #Errors with the connection
-            self.pub.publish(self.SetState)
             self.Reconnection()
         if self.IP_FOUND:       #Si se encuentra enviara directamente los archivos
-            if self.state == 5:
-                self.CP_files = subprocess.Popen(['cp','-r','/home/asus/Desktop/Catkin_ws/src/shared_folders/Backup/','/home/asus/Desktop/Test/'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                rospy.loginfo('Done')
-
-        print self.state
-        self.SetState = 0 #Esperando a recibir una entrada o simplemente esta vacia la variable
-        self.pub.publish(self.SetState) #Resetea el topico
+            self.Send
+            print 'Done'
 
 if __name__=="__main__":
     Windows = Server()
-    Rate = rospy.Rate(2)
+    Rate = rospy.Rate(5)
     ctrl_c = False
 
     def shutdownhook():
